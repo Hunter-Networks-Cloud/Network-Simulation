0d5e0ba282991ae939d3eca7149c3fc489878158
ReasonableSolutions <wg.kaming@gmail.com>
2019-12-11 15:56:26 -0500

    Moved formatting around

--- baseStation.py
+++ baseStation.py
@@ -1,7 +1,7 @@
 
 class BaseStation:
     def __init__(self, base_name, x, y, r):
+        self.id = base_name        
         self.x = x
         self.y = y
         self.radius = r
-        self.id = base_name
