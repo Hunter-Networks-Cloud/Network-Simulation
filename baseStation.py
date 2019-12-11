ebdb8cc49bb0c51ebd8b4c33d986608314d31522
ReasonableSolutions <wg.kaming@gmail.com>
2019-12-11 18:27:09 -0500

    Change x/y storage to 2-tuple, removed channel quality from node

--- baseStation.py
+++ baseStation.py
@@ -2,6 +2,5 @@
 class BaseStation:
     def __init__(self, base_name, x, y, r):
         self.id = base_name        
-        self.x = x
-        self.y = y
+        self.pos = (x, y)
         self.radius = r
--- node.py
+++ node.py
@@ -1,33 +1,24 @@
 #!/usr/bin/env python3
-
-import numpy as nm
 #-------------node class-------------
 class Node:
     def __init__(self, node_name, x, y, r):
         self.id = node_name        
-        self.x = x
-        self.y = y
+        self.pos = (x, y)
         self.radius = r
 
         #list of neighbors
         self.adjacent = []
-
-        #dictionary of channels; key=channel number(0-9), value=channel quality
-        self.channels = {}
-        for i in range(10):
-            #primary arrival is distributed exponentially, so inverse of that is channel quality
-            self.channels[i] = 1/nm.random.exponential(1, None)
     
     def __str__(self):
         return str(self.id) + ' adjacent: ' + str([x.id for x in self.adjacent])
         
 
-    def add_neighbor(self, neighbor):
+    def addNeighbor(self, neighbor):
         self.adjacent.append(neighbor)
 
     #returns [list?] of keys of adjacent nodes
-    def get_connections(self):
+    def getConnections(self):
         return self.adjacent.keys()  
 
-    def get_id(self):
+    def getId(self):
         return self.id
