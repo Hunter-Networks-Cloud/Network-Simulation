98a6c935733baf0c1ecd3b738e02b27fc989c251
ReasonableSolutions <wg.kaming@gmail.com>
2019-12-11 15:58:48 -0500

    Added radius field and input to class

--- node.py
+++ node.py
@@ -3,25 +3,29 @@
 import numpy as nm
 #-------------node class-------------
 class Node:
-    def __init__(self,x,y,node_name):
+    def __init__(self, node_name, x, y, r):
+        self.id = node_name        
         self.x = x
         self.y = y
-        self.id = node_name
+        self.radius = r
+
         #list of neighbors
         self.adjacent = []
+
         #dictionary of channels; key=channel number(0-9), value=channel quality
         self.channels = {}
         for i in range(10):
             #primary arrival is distributed exponentially, so inverse of that is channel quality
             self.channels[i] = 1/nm.random.exponential(1, None)
-      
+    
     def __str__(self):
         return str(self.id) + ' adjacent: ' + str([x.id for x in self.adjacent])
         
 
-    def add_neighbor(self, neighbor, weight=0):
+    def add_neighbor(self, neighbor):
         self.adjacent.append(neighbor)
 
+    #returns [list?] of keys of adjacent nodes
     def get_connections(self):
         return self.adjacent.keys()  
 
